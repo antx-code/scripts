5ada13f23d8e961f725714fca9b5673669e033e6
antx-code <wkaifeng2007@163.com>
2023-04-18 14:59:00 +0800

    update dwmbar-binance

--- dwmbar-functions/dwm_binance.sh
+++ dwmbar-functions/dwm_binance.sh
@@ -1,30 +1,59 @@
 #!/bin/sh
+# shellcheck disable=SC1083
+# shellcheck disable=SC2086
 
 # A dwm_bar function that shows the btc perp last price on binance
 # antx-code <wkaifeng2007@163.com>
 # GNU GPLv3
 
 # Last price is formatted like like this: "[₿ 20890.8]"
+
 binance_kline="https://dapi.binance.com/dapi/v1/ticker/24hr?symbol=BTCUSD_PERP"
 btc_icon="₿"
-dwm_binance () {
-    http_code=`curl --proxy http://127.0.0.1:7890 -I -m 10 -o /dev/null -s -w %{http_code} $binance_kline`
+PRICE_SCALE=1
+PROXY_PORT=7890
 
-    if [ "$http_code" -eq "405" ]
-    then
-#      last_price=`curl -s $binance_kline | jq -r '.[0].lastPrice'`
-      last=`curl --proxy http://127.0.0.1:7890 -v --stderr - $binance_kline | grep -oP '(?<="lastPrice":")[^"]*'`
-      last_price=`awk -v x=1 -v y=$last 'BEGIN{printf "%.2f\n",x*y}'`
+# Outputs BTC price on dwm status bar
+dwm_binance() {
+    if [ "$(check_http_code)" -eq "405" ]; then
+        btc_last_price=$(get_last_price)
+    elif [ "$(check_http_code true)" -eq "405" ]; then
+        btc_last_price=$(get_last_price true)
     else
-      last_price="NError"
+        printf "NError"
     fi
+    # Format output
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "$btc_icon %s" "$last_price"
+        printf "$btc_icon %s" "$btc_last_price"
     else
-        printf "BTC %s" "$last_price"
+        printf "BTC %s" "$btc_last_price"
     fi
     printf "%s\n" "$SEP2"
+}
+
+
+execute_curl() {
+    use_proxy=${1:-false}
+    proxy_cmd=$(echo "$use_proxy" | grep -iq "true" && echo "--proxy http://127.0.0.1:$PROXY_PORT")
+    cmd="$2"
+    curl_cmd="curl $proxy_cmd $cmd \"$binance_kline\""
+    echo "$curl_cmd"
+}
+
+check_http_code() {
+    curl_cmd="-I -m 10 -o /dev/null -s -w %{http_code}"
+    request_cmd=$(execute_curl "$1" "$curl_cmd")
+    response=$(eval $request_cmd)
+    echo $response
+}
+
+get_last_price() {
+    curl_cmd="-v --stderr -"
+    request_cmd=$(execute_curl "$1" "$curl_cmd")
+    last=$(eval $request_cmd | grep -oP '(?<="lastPrice":")[^"]*')
+    btc_last_price=$(awk -v x=$PRICE_SCALE -v y=$last 'BEGIN{printf "%.2f\n",x*y}')
+    echo $btc_last_price
 }
 
 dwm_binance
