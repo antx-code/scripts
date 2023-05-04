f381481bf60caf6a82873cea589f89ee0a4ee242
antx-code <wkaifeng2007@163.com>
2023-05-04 15:51:55 +0800

    update burpsuitepro start method for manjaro

--- autoburp.sh
+++ autoburp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nohup java -Dfile.encoding=utf-8 -javaagent:/home/antx-soc/Downs/Hacks/burpsuite/BurpSuiteLoader_v2021.9.jar -noverify -jar /home/antx-soc/Downs/Hacks/burpsuite/burpsuite_pro_v2021.9.jar > ~/scripts/logs/burp.log 2>&1 &
+nohup java -noverify -Dsun.java2d.d3d=false -Dsun.java2d.noddraw=true --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED -javaagent:/home/antx-soc/Downs/Hack/BurpSuitePro/BurpSuiteLoader.jar -jar /home/antx-soc/BurpSuitePro/burpsuite_pro.jar > ~/scripts/logs/burp.log 2>&1 &
